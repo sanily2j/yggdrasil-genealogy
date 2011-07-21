cfc5c389b58d4e073299adf3b79c394c2b0cff9c
leif.biberg.kristensen@gmail.com <leif.biberg.kristensen@gmail.com@b75f04b9-f015-8a53-7ad4-f80684a5de27>
2011-07-21 09:14:26 +0000

    Applied patches from Sandra and a few other bug fixes

--- trunk/langs/nb_canon.php
+++ trunk/langs/nb_canon.php
@@ -1,5 +1,28 @@
 <?php
 
+/***************************************************************************
+ *   nb_canon.php                                                          *
+ *   Yggdrasil: Norwegian "canonical" names                                *
+ *                                                                         *
+ *   Copyright (C) 2009-2011 by Leif B. Kristensen                         *
+ *   leif@solumslekt.org                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 // Split from source_search.php 2011-07-20
 
 // NOTE: The following function and paragraph is a preliminary implementation.
