01397a678e3d1d63c74126e49ca10f7d3d71931b
leif.biberg.kristensen@gmail.com <leif.biberg.kristensen@gmail.com@b75f04b9-f015-8a53-7ad4-f80684a5de27>
2011-12-14 08:59:45 +0000

    Changed license from GNU to Simplified BSD, modified license note in every file

--- trunk/forms/person_delete.php
+++ trunk/forms/person_delete.php
@@ -4,23 +4,8 @@
  *   person_delete.php                                                     *
  *   Yggdrasil: Delete Person Action                                       *
  *                                                                         *
- *   Copyright (C) 2006-2011 by Leif B. Kristensen                         *
- *   leif@solumslekt.org                                                   *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   Copyright (C) 2006-2011 by Leif B. Kristensen <leif@solumslekt.org>   *
+ *   All rights reserved. For terms of use, see LICENSE.txt                *
  ***************************************************************************/
 
 // Note: This script will delete a person. It should not be used if a person
